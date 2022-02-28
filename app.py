528a9b74ffdb960678bffb60a6ef6c49bb08a8a1
Gennady Temnikov <zerochip@gmail.com>
2022-02-28 11:17:43 -0800

    Add Procfile and gunicorn dependency

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def base():
+    return "up"
+
+
 @app.route('/basepath')
 def index():
     return render_template('index.html')
+
